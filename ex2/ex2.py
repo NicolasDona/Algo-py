d70501a63fbc35e64be9a07db4f6ded70c17ec26
NicolasDona <nicolas.donascimento@gmail.com>
2023-09-25 16:43:50 +0200

    save

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -4,6 +4,7 @@ nombre2 = float(input("Entrez le deuxième nombre : "))
 
 # nombre > 0 vérifie si le deuxième nombre est strictement positif.
 # nombre < 0 vérifie si le deuxième nombre est strictement négatif.
+# bien faire la différence pour les deux cas entre les 2 ()
 if (nombre1 > 0 and nombre2 > 0) or (nombre1 < 0 and nombre2 < 0):
     resultat = "positif"
 else:
