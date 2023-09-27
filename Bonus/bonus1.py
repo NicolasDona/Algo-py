03fb3b6afee40e7610731fe0a2dd56a957ecc446
NicolasDona <nicolas.donascimento@gmail.com>
2023-09-27 16:41:27 +0200

    save bonus

---
+++ Bonus/bonus1.py
@@ -0,0 +1,21 @@
+# bonus algo:
+
+# Soit un tableau contenant 10 nombres.
+# Écrire un algorithme qui permettra de faire la multiplication de ces 10 nombres entre eux
+# et d'affecter le résultat à une variable resultCalc
+
+# autre bonus:
+
+# Écrire un algorithme qui permettra de faire le modulo de 2 nombres, puis de multiplier le résultat par 60.
+# Le calcul du modulo devra se faire dans une fonction ModuloNumbers qui sera à appeler dans l'algorithme
+# principa
+
+# et encore:
+
+# Écrire un algorithme qui demande à l'utilisateur son âge puis son genre.
+# En fonction des réponses de celui-ci, afficher une phrase telle que :
+# "Bonjour Madame. Vous êtes majeure."
+# PS : On se limitera pour cet exercice à Homme et Femme
+
+numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+resultCalc = 
